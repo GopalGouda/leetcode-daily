c359599874935ec899ea3dd09be63d84581774d6
GopalGouda <thegopalgouda@gmail.com>
2025-07-01 10:12:02 +0530

    Added first solution: 001_two_sum.cpp

---
+++ solutions/001_two_sum.cpp
@@ -0,0 +1,16 @@
+// Problem: Two Sum
+// Link: https://leetcode.com/problems/two-sum/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i = 0; i <= nums.size() - 2; i++) {
+            for(int j = i + 1; j <= nums.size() - 1; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return {i, j};  //Return the indices as a vector
+                }
+            }
+        }
+        return {}; // Return empty vector if no solution
+    }
+};
