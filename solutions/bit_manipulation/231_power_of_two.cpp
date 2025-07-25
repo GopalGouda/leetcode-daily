681ae710bcc89b32cbbf9a0593cc94c9b1328721
GopalGouda <thegopalgouda@gmail.com>
2025-07-25 22:39:59 +0530

    Power of Two question solved

--- solutions/bit_manipulation/231_power_of_two.cpp
+++ solutions/bit_manipulation/231_power_of_two.cpp
@@ -3,7 +3,7 @@
 // 🗂 Topic: Bit Manipulation
 // ⏱ Time Complexity: O(1)
 // 💾 Space Complexity: O(1)
-// 🧠 Approach: Bit Manipulation
+// 🧠 Approach: Bit Manipulation (best approch)
 
 #include <iostream>
 #include <vector>
@@ -12,13 +12,18 @@ using namespace std;
 
 class Solution {
 public:
-    bool isPowerOfTwo(int n) {
+    bool isPowerOfTwo(int n) { //Bit manipulation method
 
         if (n <= 0) return false;
 
         // If n is a power of two, it will have exactly one bit set
         // So n & (n - 1) should be 0
         return (n & (n - 1)) == 0;
+    }
+    bool isPowerOfTwo(int n) {  //Recursion Method
+        if (n == 1) return true;
+        if (n <= 0 || n % 2 != 0) return false;
+        return isPowerOfTwo(n / 2);
     }
 };
 
