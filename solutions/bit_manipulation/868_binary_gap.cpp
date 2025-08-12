8cd700b6fc79f36ce5a7133190fbabaa06eaa3df
GopalGouda <thegopalgouda@gmail.com>
2025-08-12 21:41:00 +0530

    Binary Gap question solved

--- solutions/bit_manipulation/868_binary_gap.cpp
+++ solutions/bit_manipulation/868_binary_gap.cpp
@@ -12,8 +12,26 @@ using namespace std;
 
 class Solution {
 public:
-    // Your code here
+    int binaryGap(int n) {
+        int lastPos = -1;       // position of last seen '1'
+        int maxDist = 0;        // max distance found
+        int pos = 0;            // current bit position
+        
+        while (n > 0) {
+            if (n & 1) {        // check if current bit is '1'
+                if (lastPos != -1) {
+                    maxDist = max(maxDist, pos - lastPos);
+                }
+                lastPos = pos;  // update last seen '1'
+            }
+            n >>= 1;            // shift to next bit
+            pos++;              // move to next bit position
+        }
+        
+        return maxDist;
+    }
 };
+
 
 int main() {
     Solution sol;
