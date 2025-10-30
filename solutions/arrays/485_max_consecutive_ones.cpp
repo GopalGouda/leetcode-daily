4c882aad09dba1b60ed29ee0b1e81026d2121463
GopalGouda <thegopalgouda@gmail.com>
2025-10-30 23:36:37 +0530

    Max Consecutive Ones question's answer updates

--- solutions/arrays/485_max_consecutive_ones.cpp
+++ solutions/arrays/485_max_consecutive_ones.cpp
@@ -14,17 +14,17 @@ class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
         int count=0;
-        int total=0;
+        int ans=0;
         
         for(int i=0; i<nums.size(); i++){
             if(nums[i]==1) count++;
             else {
-                total= max(count, total);
+                ans= max(count, ans);
                 count=0;
             }
         }
-        total= max(count, total);
-        return total;
+        ans= max(count, ans);
+        return ans;
     }
 };
 
