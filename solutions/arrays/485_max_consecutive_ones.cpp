3c940c2406af6c713aa5df81c37f43abfdf2576c
GopalGouda <thegopalgouda@gmail.com>
2025-10-30 23:25:45 +0530

    Max Consecutive Ones question solved

---
+++ solutions/arrays/485_max_consecutive_ones.cpp
@@ -0,0 +1,35 @@
+// ✅ Problem: Max Consecutive Ones
+// 🔗 Link: https://leetcode.com/problems/max-consecutive-ones/description/?envType=problem-list-v2&envId=n8ahh1k5
+// 🗂 Topic: arrays
+// ⏱ Time Complexity: O(N)
+// 💾 Space Complexity: O(1)
+// 🧠 Approach: 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count=0;
+        int total=0;
+        
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==1) count++;
+            else {
+                total= max(count, total);
+                count=0;
+            }
+        }
+        total= max(count, total);
+        return total;
+    }
+};
+
+int main() {
+    Solution sol;
+    // Test cases
+    return 0;
+}
