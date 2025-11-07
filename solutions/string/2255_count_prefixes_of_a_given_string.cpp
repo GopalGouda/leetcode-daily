5e331e700f0284ab5fc35f34ce0538c76acfa713
Gopal <thegopalgouda@gmail.com>
2025-11-07 23:45:57 +0530

    Count Prefixes of a Given String question solved

---
+++ solutions/string/2255_count_prefixes_of_a_given_string.cpp
@@ -0,0 +1,30 @@
+// ✅ Problem: Count Prefixes of a Given String
+// 🔗 Link: https://leetcode.com/problems/count-prefixes-of-a-given-string/description/?envType=problem-list-v2&envId=n8ahh1k5
+// 🗂 Topic: string
+// ⏱ Time Complexity: O(N*M)
+// 💾 Space Complexity: O(1)
+// 🧠 Approach: 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int countPrefixes(vector<string>& words, string s) {
+    int count = 0;
+        for (auto& word : words) {
+            if (s.rfind(word, 0) == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+int main() {
+    Solution sol;
+    // Test cases
+    return 0;
+}
