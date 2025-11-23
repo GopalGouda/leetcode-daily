0c6b674b6366a8c62bbff5621375478deb3ecff5
Gopal <thegopalgouda@gmail.com>
2025-11-23 21:10:17 +0530

    Generate a String With Characters That Have Odd Counts question solved

---
+++ solutions/string/1374_generate_a_string_with_characters_that_have_odd_counts.cpp
@@ -0,0 +1,28 @@
+// ✅ Problem: Generate a String With Characters That Have Odd Counts
+// 🔗 Link: https://leetcode.com/problems/generate-a-string-with-characters-that-have-odd-counts/description/
+// 🗂 Topic: string
+// ⏱ Time Complexity: O()
+// 💾 Space Complexity: O()
+// 🧠 Approach: 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string generateTheString(int n) {
+        if (n % 2 == 1) {
+            return string(n, 'a');   // all 'a's
+        } else {
+            return string(n - 1, 'a') + 'b';   // n-1 'a's + 1 'b'
+        }       
+    }
+};
+
+int main() {
+    Solution sol;
+    // Test cases
+    return 0;
+}
