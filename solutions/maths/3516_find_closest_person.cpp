e294ea2b22d7a48baedcab28af4515a0b553870f
Gopal <thegopalgouda@gmail.com>
2025-12-08 23:50:53 +0530

    Find Closest Person question solved

---
+++ solutions/maths/3516_find_closest_person.cpp
@@ -0,0 +1,28 @@
+// ✅ Problem: Find Closest Person
+// 🔗 Link: https://leetcode.com/problems/find-closest-person/description/
+// 🗂 Topic: maths
+// ⏱ Time Complexity: O()
+// 💾 Space Complexity: O()
+// 🧠 Approach: 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int dx = abs(x - z);
+        int dy = abs(y - z);
+        if (dx < dy) return 1;
+        if (dx > dy) return 2;
+        return 0;        
+    }
+};
+
+int main() {
+    Solution sol;
+    // Test cases
+    return 0;
+}
