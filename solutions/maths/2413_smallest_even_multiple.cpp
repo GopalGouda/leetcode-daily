398d0c44331b8fa0facecb89b9a46b3595ff5461
Gopal <thegopalgouda@gmail.com>
2025-12-09 23:05:57 +0530

    Smallest Even Multiple question solved

---
+++ solutions/maths/2413_smallest_even_multiple.cpp
@@ -0,0 +1,24 @@
+// ✅ Problem: Smallest Even Multiple
+// 🔗 Link: https://leetcode.com/problems/smallest-even-multiple/description/
+// 🗂 Topic: maths
+// ⏱ Time Complexity: O()
+// 💾 Space Complexity: O()
+// 🧠 Approach: 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+    return (n % 2 == 0) ? n : 2 * n; //(condition) ? value_if_true : value_if_false   
+    }
+};
+
+int main() {
+    Solution sol;
+    // Test cases
+    return 0;
+}
