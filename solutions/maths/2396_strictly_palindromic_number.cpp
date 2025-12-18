0afb991febc778a4e8b90c06188053fd71eec12f
Gopal <thegopalgouda@gmail.com>
2025-12-18 22:27:17 +0530

    Strictly Palindromic Number question solved

---
+++ solutions/maths/2396_strictly_palindromic_number.cpp
@@ -0,0 +1,24 @@
+// ✅ Problem: Strictly Palindromic Number
+// 🔗 Link: https://leetcode.com/problems/strictly-palindromic-number/description/
+// 🗂 Topic: maths
+// ⏱ Time Complexity: O(1)
+// 💾 Space Complexity: O(1)
+// 🧠 Approach: 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+    return false;    
+    }
+};
+
+int main() {
+    Solution sol;
+    // Test cases
+    return 0;
+}
