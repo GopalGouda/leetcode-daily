abcb12b28074a2e46571016fb0fe24b13aa33bcb
Gopal <thegopalgouda@gmail.com>
2025-12-30 18:58:35 +0530

    Maximum 69 Number question solved

---
+++ solutions/string/1323_maximum_69_number.cpp
@@ -0,0 +1,31 @@
+// ✅ Problem: Maximum 69 Number
+// 🔗 Link: https://leetcode.com/problems/maximum-69-number/description/
+// 🗂 Topic: string
+// ⏱ Time Complexity: O(N)
+// 💾 Space Complexity: O(N)
+// 🧠 Approach: Convert the number to a string, iterate through each character, and replace the first '6' with '9' to maximize the value.
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string s = to_string(num);
+        for (char &c : s) {
+            if (c == '6') {
+                c = '9';
+                break;
+            }
+        }
+        return stoi(s);        
+    }
+};
+
+int main() {
+    Solution sol;
+    // Test cases
+    return 0;
+}
