ef6ed0ef8d1776486321054a554c6a534bc41f63
Gopal <thegopalgouda@gmail.com>
2026-01-01 23:51:25 +0530

    Find the Number of Good Pairs I question solved

---
+++ solutions/arrays/3162_find_the_number_of_good_pairs_i.cpp
@@ -0,0 +1,22 @@
+// ✅ Problem: Find the Number of Good Pairs I
+// 🔗 Link: https://leetcode.com/problems/find-the-number-of-good-pairs-i/description/
+// 🗂 Topic: arrays
+// ⏱ Time Complexity: O()
+// 💾 Space Complexity: O()
+// 🧠 Approach: 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    // Your code here
+};
+
+int main() {
+    Solution sol;
+    // Test cases
+    return 0;
+}
