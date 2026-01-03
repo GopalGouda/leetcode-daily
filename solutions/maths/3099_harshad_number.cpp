1c5ee531ae279c1d62f08cd2ce961df53bc34b3c
Gopal <thegopalgouda@gmail.com>
2026-01-03 11:25:13 +0530

    Harshad Number question solved

---
+++ solutions/maths/3099_harshad_number.cpp
@@ -0,0 +1,30 @@
+// ✅ Problem: Harshad Number
+// 🔗 Link: https://leetcode.com/problems/harshad-number/description/
+// 🗂 Topic: maths
+// ⏱ Time Complexity: O(N)
+// 💾 Space Complexity: O(1)
+// 🧠 Approach: 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0, temp = x;
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        return (x % sum == 0) ? sum : -1;
+        
+    }
+};
+
+int main() {
+    Solution sol;
+    // Test cases
+    return 0;
+}
