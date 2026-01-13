bad227f5309b0749cf71949ac6403897efb1bf04
Gopal <thegopalgouda@gmail.com>
2026-01-13 09:14:12 +0530

    Check if a String Is an Acronym of Words question solved

--- solutions/string/2828_check_if_a_string_is_an_acronym_of_words.cpp
+++ solutions/string/2828_check_if_a_string_is_an_acronym_of_words.cpp
@@ -1,8 +1,8 @@
 // ✅ Problem: Check if a String Is an Acronym of Words
 // 🔗 Link: https://leetcode.com/problems/check-if-a-string-is-an-acronym-of-words/description/?envType=problem-list-v2&envId=n8ahh1k5
 // 🗂 Topic: string
-// ⏱ Time Complexity: O()
-// 💾 Space Complexity: O()
+// ⏱ Time Complexity: O(N)
+// 💾 Space Complexity: O(1)
 // 🧠 Approach: 
 
 #include <iostream>
@@ -12,7 +12,17 @@ using namespace std;
 
 class Solution {
 public:
-    // Your code here
+    bool isAcronym(vector<string>& words, string s) {
+        if (words.size() != s.length())
+            return false;
+
+        for (int i = 0; i < words.size(); i++) {
+            if (words[i][0] != s[i])
+                return false;
+        }
+
+        return true;
+    }
 };
 
 int main() {
