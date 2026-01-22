a99be76b15fee1ab59a0ffee1c20483dcdd0f6c6
Gopal <thegopalgouda@gmail.com>
2026-01-22 22:56:22 +0530

    Restore Finishing Order question solved

--- solutions/hash_table/3668_restore_finishing_order.cpp
+++ solutions/hash_table/3668_restore_finishing_order.cpp
@@ -1,20 +1,29 @@
 // ✅ Problem: Restore Finishing Order
 // 🔗 Link: https://leetcode.com/problems/restore-finishing-order/description/
 // 🗂 Topic: hash_table
-// ⏱ Time Complexity: O()
-// 💾 Space Complexity: O()
+// ⏱ Time Complexity: O(N)
+// 💾 Space Complexity: O(N)
 // 🧠 Approach: 
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <unordered_set>
 using namespace std;
 
 class Solution {
 public:
-    // Your code here
-};
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        unordered_set<int> isFriend(friends.begin(), friends.end());
+        vector<int> result;
 
+        for (int id : order) {
+            if (isFriend.count(id)) {
+                result.push_back(id);
+            }
+        }
+        return result;        
+    }
+};
 int main() {
     Solution sol;
     // Test cases
