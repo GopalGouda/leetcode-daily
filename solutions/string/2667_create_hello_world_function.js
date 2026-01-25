ecce3519d92ad712cc73aae57d1ff4ca79ab1207
Gopal <thegopalgouda@gmail.com>
2026-01-25 19:47:05 +0530

    Create Hello World Function question solved

---
+++ solutions/string/2667_create_hello_world_function.js
@@ -0,0 +1,10 @@
+// ✅ Problem: Create Hello World Function
+// 🔗 Link: https://leetcode.com/problems/create-hello-world-function/description/
+// 🗂 Topic: string
+// ⏱ Time Complexity: O(1)
+// 💾 Space Complexity: O(1)
+// 🧠 Approach: 
+
+var createHelloWorld = function() {
+    return () => "Hello World";
+}
