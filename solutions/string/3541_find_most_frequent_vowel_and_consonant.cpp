69d4245aa67299a1be7da596bcfc1b11300c82d5
Gopal <thegopalgouda@gmail.com>
2026-02-17 22:52:40 +0530

    Find Most Frequent Vowel and Consonant question solved

--- solutions/string/3541_find_most_frequent_vowel_and_consonant.cpp
+++ solutions/string/3541_find_most_frequent_vowel_and_consonant.cpp
@@ -1,9 +1,9 @@
 // ✅ Problem: Find Most Frequent Vowel and Consonant
 // 🔗 Link: https://leetcode.com/problems/find-most-frequent-vowel-and-consonant/description/
 // 🗂 Topic: string
-// ⏱ Time Complexity: O()
-// 💾 Space Complexity: O()
-// 🧠 Approach: 
+// ⏱ Time Complexity: O(N)
+// 💾 Space Complexity: O(1)
+// 🧠 Approach:
 
 #include <iostream>
 #include <vector>
@@ -12,7 +12,30 @@ using namespace std;
 
 class Solution {
 public:
-    // Your code here
+    int maxFreqSum(string s) {
+        vector<int> freq(26, 0);
+        
+        // Count frequencies
+        for (char c : s) {
+            freq[c - 'a']++;
+        }
+        
+        int maxVowel = 0;
+        int maxConsonant = 0;
+        
+        for (int i = 0; i < 26; i++) {
+            char c = 'a' + i;
+            
+            if (c == 'a' || c == 'e' || c == 'i' || 
+                c == 'o' || c == 'u') {
+                maxVowel = max(maxVowel, freq[i]);
+            } else {
+                maxConsonant = max(maxConsonant, freq[i]);
+            }
+        }
+        
+        return maxVowel + maxConsonant;        
+    }
 };
 
 int main() {
