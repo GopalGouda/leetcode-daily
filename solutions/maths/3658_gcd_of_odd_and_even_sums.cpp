14d6814c50911c8e7da395a5888a8fc45ecde56a
Gopal <thegopalgouda@gmail.com>
2026-02-19 22:02:46 +0530

    GCD of Odd and Even Sums question solved

---
+++ solutions/maths/3658_gcd_of_odd_and_even_sums.cpp
@@ -0,0 +1,24 @@
+// ✅ Problem: GCD of Odd and Even Sums
+// 🔗 Link: https://leetcode.com/problems/gcd-of-odd-and-even-sums/description/
+// 🗂 Topic: maths
+// ⏱ Time Complexity: O(1)
+// 💾 Space Complexity: O(1)
+// 🧠 Approach: 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;  
+    }
+};
+
+int main() {
+    Solution sol;
+    // Test cases
+    return 0;
+}
