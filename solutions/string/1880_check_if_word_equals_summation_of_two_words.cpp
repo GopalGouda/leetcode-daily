9a6f9182c7ae96d699c0ac18faad1ceab956f087
Gopal <thegopalgouda@gmail.com>
2026-02-23 22:58:45 +0530

    Check if Word Equals Summation of Two Words question solved

--- solutions/string/1880_check_if_word_equals_summation_of_two_words.cpp
+++ solutions/string/1880_check_if_word_equals_summation_of_two_words.cpp
@@ -1,8 +1,8 @@
 // ✅ Problem: Check if Word Equals Summation of Two Words
 // 🔗 Link: https://leetcode.com/problems/check-if-word-equals-summation-of-two-words/description/
 // 🗂 Topic: string
-// ⏱ Time Complexity: O()
-// 💾 Space Complexity: O()
+// ⏱ Time Complexity: O(N)
+// 💾 Space Complexity: O(1)
 // 🧠 Approach: 
 
 #include <iostream>
@@ -12,7 +12,17 @@ using namespace std;
 
 class Solution {
 public:
-    // Your code here
+    int convert(string word) {
+        string num = "";
+        for(char ch : word) {
+            num += to_string(ch - 'a');
+        }
+        return stoi(num);
+    }
+    
+    bool isSumEqual(string firstWord, string secondWord, string targetWord) {
+        return convert(firstWord) + convert(secondWord) == convert(targetWord);
+    }
 };
 
 int main() {
