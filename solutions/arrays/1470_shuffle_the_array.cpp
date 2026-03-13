8c7a3560abd16e7c667257b8d4522e97a079ad02
Gopal <thegopalgouda@gmail.com>
2026-03-13 11:09:10 +0530

    Shuffle the Array qustuion sovled

--- solutions/arrays/1470_shuffle_the_array.cpp
+++ solutions/arrays/1470_shuffle_the_array.cpp
@@ -1,9 +1,9 @@
 // ✅ Problem: Shuffle the Array
-// 🔗 Link: https://leetcode.com/problems/shuffle-the-array/description/?envType=problem-list-v2&envId=n8ahh1k5
+// 🔗 Link: https://leetcode.com/problems/shuffle-the-array/description/
 // 🗂 Topic: arrays
 // ⏱ Time Complexity: O(N)
 // 💾 Space Complexity: O(N)
-// 🧠 Approach: Two Pointers
+// 🧠 Approach: 
 
 #include <iostream>
 #include <vector>
@@ -13,20 +13,14 @@ using namespace std;
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        int size= nums.size();
-        vector <int> ans(size);
-        int j=0, k=n;
-        for(int i=0; i<size; i++){
-            if(i%2==0){
-                ans[i]=nums[j];
-                j++;
-            }else{
-                ans[i]=nums[k];
-                k++;
-            }
+        vector<int> result;
+
+        for (int i = 0; i < n; i++) {
+            result.push_back(nums[i]);
+            result.push_back(nums[i + n]);
         }
-        return ans;
-        
+
+        return result;        
     }
 };
 
