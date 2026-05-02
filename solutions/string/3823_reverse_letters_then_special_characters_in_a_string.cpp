5aae8e9562ce8b63ece208a9f86438f26b6a8de7
Gopal <thegopalgouda@gmail.com>
2026-05-02 22:51:07 +0530

    class Solution {

--- solutions/string/3823_reverse_letters_then_special_characters_in_a_string.cpp
+++ solutions/string/3823_reverse_letters_then_special_characters_in_a_string.cpp
@@ -1,8 +1,8 @@
 // ✅ Problem: Reverse Letters Then Special Characters in a String
 // 🔗 Link: https://leetcode.com/problems/reverse-letters-then-special-characters-in-a-string/description/
 // 🗂 Topic: string
-// ⏱ Time Complexity: O()
-// 💾 Space Complexity: O()
+// ⏱ Time Complexity: O(N)
+// 💾 Space Complexity: O(N)
 // 🧠 Approach: 
 
 #include <iostream>
@@ -12,7 +12,36 @@ using namespace std;
 
 class Solution {
 public:
-    // Your code here
+    string reverseByType(string s) {
+vector<char> letters, specials;
+
+    // Step 1: Collect characters
+    for (char c : s) {
+        if (islower(c)) {
+            letters.push_back(c);
+        } else {
+            specials.push_back(c);
+        }
+    }
+
+    // Step 2: Reverse both groups
+    reverse(letters.begin(), letters.end());
+    reverse(specials.begin(), specials.end());
+
+    // Step 3: Rebuild string
+    int i = 0, j = 0;
+    string result = "";
+
+    for (char c : s) {
+        if (islower(c)) {
+            result += letters[i++];
+        } else {
+            result += specials[j++];
+        }
+    }
+
+    return result;        
+    }
 };
 
 int main() {
