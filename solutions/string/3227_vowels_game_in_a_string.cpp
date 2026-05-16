b6445549b3df1d30a3a652ff2789bc357e2b7518
Gopal <thegopalgouda@gmail.com>
2026-05-16 23:09:27 +0530

    Vowels Game in a String question solved

--- solutions/string/3227_vowels_game_in_a_string.cpp
+++ solutions/string/3227_vowels_game_in_a_string.cpp
@@ -1,8 +1,8 @@
 // ✅ Problem: Vowels Game in a String
 // 🔗 Link: https://leetcode.com/problems/vowels-game-in-a-string/description/
 // 🗂 Topic: string
-// ⏱ Time Complexity: O()
-// 💾 Space Complexity: O()
+// ⏱ Time Complexity: O(n)
+// 💾 Space Complexity: O(1)
 // 🧠 Approach: 
 
 #include <iostream>
@@ -12,7 +12,19 @@ using namespace std;
 
 class Solution {
 public:
-    // Your code here
+    bool doesAliceWin(string s) {
+        for (char c : s) {
+            // If there is at least one vowel,
+            // Alice can always win
+            if (c == 'a' || c == 'e' || c == 'i' || 
+                c == 'o' || c == 'u') {
+                return true;
+            }
+        }
+
+        // No vowels => Alice has no valid move
+        return false;        
+    }
 };
 
 int main() {
