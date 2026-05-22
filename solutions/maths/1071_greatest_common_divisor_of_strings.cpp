dd153f91fa57e7449e37cd141aafcc8c77a314cc
Gopal <thegopalgouda@gmail.com>
2026-05-22 22:44:20 +0530

    solved Greatest Common Divisor of Strings using gcd

--- solutions/maths/1071_greatest_common_divisor_of_strings.cpp
+++ solutions/maths/1071_greatest_common_divisor_of_strings.cpp
@@ -8,28 +8,21 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
+
 using namespace std;
 
 class Solution {
 public:
-    int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
     string gcdOfStrings(string str1, string str2) {
         // If no common divisor string exists
         if (str1 + str2 != str2 + str1)
             return "";
-
+        
         // Find gcd of lengths
         int len = gcd(str1.size(), str2.size());
-
+        
         // Return the common prefix
-        return str1.substr(0, len);
+        return str1.substr(0, len);        
     }
 };
 
