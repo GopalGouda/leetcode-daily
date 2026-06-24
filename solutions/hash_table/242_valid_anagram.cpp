f0fc2a174e061eb7c4d4abb11bd3ce95282e84af
Gopal <thegopalgouda@gmail.com>
2026-06-24 23:37:26 +0530

    Valid Anagram question solved

---
+++ solutions/hash_table/242_valid_anagram.cpp
@@ -0,0 +1,37 @@
+// ✅ Problem: Valid Anagram
+// 🔗 Link: https://leetcode.com/problems/valid-anagram/description/
+// 🗂 Topic: hash_table
+// ⏱ Time Complexity: O()
+// 💾 Space Complexity: O()
+// 🧠 Approach: 
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+
+        unordered_map<char, int> freq;
+
+        for (char c : s) {
+            freq[c]++;
+        }
+
+        for (char c : t) {
+            freq[c]--;
+            if (freq[c] < 0) return false;
+        }
+
+        return true;        
+    }
+};
+
+int main() {
+    Solution sol;
+    // Test cases
+    return 0;
+}
