55e8a2f960966f6d85e827387d432cf1a282ea91
Gopal <thegopalgouda@gmail.com>
2026-08-06 19:52:01 +0530

    Check if Number Has Equal Digit Count and Digit Value question solved

--- solutions/hash_table/2283_check_if_number_has_equal_digit_count_and_digit_value.cpp
+++ solutions/hash_table/2283_check_if_number_has_equal_digit_count_and_digit_value.cpp
@@ -1,9 +1,9 @@
 // ✅ Problem: Check if Number Has Equal Digit Count and Digit Value
 // 🔗 Link: https://leetcode.com/problems/check-if-number-has-equal-digit-count-and-digit-value/description/
 // 🗂 Topic: hash_table
-// ⏱ Time Complexity: O()
-// 💾 Space Complexity: O()
-// 🧠 Approach: 
+// ⏱ Time Complexity: O(n)
+// 💾 Space Complexity: O(1)
+// 🧠 Approach:
 
 #include <iostream>
 #include <vector>
@@ -12,7 +12,22 @@ using namespace std;
 
 class Solution {
 public:
-    // Your code here
+    bool digitCount(string num) {
+        vector<int> freq(10, 0);
+
+        // Count frequency of each digit
+        for (char ch : num) {
+            freq[ch - '0']++;
+        }
+
+        // Verify each index
+        for (int i = 0; i < num.size(); i++) {
+            if (freq[i] != num[i] - '0')
+                return false;
+        }
+
+        return true;        
+    }
 };
 
 int main() {
