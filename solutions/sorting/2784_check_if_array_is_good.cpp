34eb037bcf07b9dbff2bea505d615f25f9074ee2
Gopal <thegopalgouda@gmail.com>
2026-09-21 23:39:59 +0530

    Check if Array is Good question solved

--- solutions/sorting/2784_check_if_array_is_good.cpp
+++ solutions/sorting/2784_check_if_array_is_good.cpp
@@ -1,8 +1,8 @@
 // ✅ Problem: Check if Array is Good
 // 🔗 Link: https://leetcode.com/problems/check-if-array-is-good/description/
 // 🗂 Topic: sorting
-// ⏱ Time Complexity: O()
-// 💾 Space Complexity: O()
+// ⏱ Time Complexity: O(n log n)
+// 💾 Space Complexity: O(n)
 // 🧠 Approach: 
 
 #include <iostream>
@@ -12,7 +12,18 @@ using namespace std;
 
 class Solution {
 public:
-    // Your code here
+    bool isGood(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size() - 1;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1)
+                return false;
+        }
+
+        return nums[n] == n;        
+    }
 };
 
 int main() {
